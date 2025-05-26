e08e026fccc364a7b6311ebf65705bfcf339f70f
isavchenkoivan <isavchenkoivan@gmail.com>
2025-05-25 22:53:23 -0400

    Initial commit

--- pytestLearning/test_parameterize.py
+++ pytestLearning/test_parameterize.py
@@ -1,4 +1,7 @@
 import pytest
+import allure
+from allure_commons.types import AttachmentType
+
 
 def get_data():
 
@@ -8,7 +11,14 @@ def get_data():
         ("info@way2automation.com", "afsdfds")
     ]
 
-
+@allure.feature("Login Test")
+@allure.severity(allure.severity_level.MINOR)
+@pytest.mark.usefixtures("log_on_failure")
 @pytest.mark.parametrize("username, password", get_data())
-def test_do_login(username, password):
-    print(f"username: {username}, password: {password}")
+def test_do_login(page, username, password):
+    with allure.step(f"Enter username and password {username}, and {password}"):
+        page.fill("#email", username)
+        page.fill("#pass", password)
+        print(f"username: {username}, password: {password}")
+        # assert 1 == 2
+    #allure.attach(page.screenshot(path="screenshot/fullpage.png"), name="dologin", attachment_type=AttachmentType.PNG)
